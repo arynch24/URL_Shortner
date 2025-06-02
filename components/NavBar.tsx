a58234fea2053b12f04bf865d898d9d75e45728e
Aryan <arynch24@gmail.com>
2025-06-02 22:48:57 +0530

    Refactor UI components for responsiveness and consistency

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,13 +3,20 @@ import { getServerSession } from 'next-auth';
 import { NEXT_AUTH_CONFIG } from '@/lib/auth';
 import Image from 'next/image';
 
+const  MenuBar = () => {
+    return (
+       <div>
+        
+       </div>
+    );
+}
+
 const DashboardNav = async () => {
     // Fetch the session to check if the user is authenticated
     const session = await getServerSession(NEXT_AUTH_CONFIG);
 
     return (
-
-        <div className="flex items-center justify-between gap-1 px-24 pr-30 py-4 border-b border-zinc-300">
+        <div className="flex items-center justify-between gap-1 px-4 md:px-24 pr-30 py-4 border-b border-zinc-300">
             <Link href='/' className="flex gap-1 items-center cursor-pointer">
                 <Image
                     src='https://res.cloudinary.com/dr8ubbrmp/image/upload/v1748000628/shoo4vbiqfjyzkmk3hyl.png'
@@ -21,12 +28,12 @@ const DashboardNav = async () => {
                 <p className="text-2xl font-semibold text-zinc-800">Cuttly</p>
             </Link>
 
-            {/* Profile component to display user information or avatar */ }
+            {/* Profile component to display user information or avatar */}
             {
                 session ?
                     null
                     :
-                    <div className="flex items-center gap-6 ">
+                    <div className="md:flex items-center gap-6 hidden ">
                         <Link href='/signin'
                             className="px-4 py-2 border border-zinc-500 rounded-md hover:bg-zinc-100 transition duration-200">
                             LogIn
